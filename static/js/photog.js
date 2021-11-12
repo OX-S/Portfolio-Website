86c5bb341b116f2c44e34c4915e4c7a7717c09a1
OX-S <finn.kliewer1@gmail.com>
2021-11-12 14:23:38 -0500

    added overlay onto images on hover

--- static/js/photog.js
+++ static/js/photog.js
@@ -5,10 +5,12 @@ function enlargeImg(e) {
     document.getElementById("largeimg").style.zIndex = "999";
     document.getElementById("large").style.zIndex = "900";
     document.getElementById("largeimg").style.display = "block";
+    document.getElementById("large").style.opacity = "60%";
 }
 
 function makeSmall() {
     document.getElementById("largeimg").style.zIndex = "-999";
     document.getElementById("large").style.zIndex = "-900";
     document.getElementById("largeimg").style.display = "none";
+    document.getElementById("large").style.opacity = "0%";
 }
