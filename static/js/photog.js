0ed8d9c0d7c6b1d7aef635fd8d1d464fff240497
OX-S <finn.kliewer1@gmail.com>
2021-11-12 01:28:40 -0500

    done for today fuck this im tired

--- static/js/photog.js
+++ static/js/photog.js
@@ -6,3 +6,9 @@ function enlargeImg(e) {
     document.getElementById("large").style.zIndex = "900";
     document.getElementById("largeimg").style.display = "block";
 }
+
+function makeSmall() {
+    document.getElementById("largeimg").style.zIndex = "-999";
+    document.getElementById("large").style.zIndex = "-900";
+    document.getElementById("largeimg").style.display = "none";
+}
